53cc5dabe4176a3569f43ebe194ed3fde170315f
michael.beddow <michael.beddow@wandisco.com>
2016-02-03 14:38:14 +0000

    Add first attempt scripts and template for ARM HDI cluster creation

---
+++ clientInstallerHN.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+clusterUN=$1
+clusterPS=$2
+clusterName=$3
+wasbURI=$4
+edgeNodeIP=$5
+
+# Import the helper method module.
+wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
+
+wget https://URI/fusion-hdi-2.2.8-client-hdfs_2.6.6-SNAPSHOT-1548_all.deb -O /tmp/hdi-client.deb
+
+dpkg -i /tmp/hdi-client.deb
+
+# if on HN
+/var/lib/ambari-server/resources/scripts/configs.sh -u $clusterUN -p $clusterPS set localhost $clusterName core-site "fusion.underlyingFs" "$wasbURI"
+/var/lib/ambari-server/resources/scripts/configs.sh -u $clusterUN -p $clusterPS set localhost $clusterName core-site "fs.wasb.impl" "org.apache.hadoop.fs.azure.NativeAzureFileSystem"
+/var/lib/ambari-server/resources/scripts/configs.sh -u $clusterUN -p $clusterPS set localhost $clusterName core-site "fs.AbstractFileSystem.fusion.impl" "com.wandisco.fs.client.FusionAbstractFs"
+/var/lib/ambari-server/resources/scripts/configs.sh -u $clusterUN -p $clusterPS set localhost $clusterName core-site "fs.fusion.server" "$edgeNodeIP:8023"
+/var/lib/ambari-server/resources/scripts/configs.sh -u $clusterUN -p $clusterPS set localhost $clusterName yarn-site "fs.AbstractFileSystem.fusion.impl" "com.wandisco.fs.client.FusionAbstractFs"
