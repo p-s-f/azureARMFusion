6f867ebc5a4ccdd21008c94a08f4c784ab296c56
michael.beddow <michael.beddow@wandisco.com>
2016-02-03 15:03:28 +0000

    update to raw github psf addresses for temp storage

--- clientInstallerHN.sh
+++ clientInstallerHN.sh
@@ -9,7 +9,7 @@ edgeNodeIP=$5
 # Import the helper method module.
 wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
 
-wget https://URI/fusion-hdi-2.2.8-client-hdfs_2.6.6-SNAPSHOT-1548_all.deb -O /tmp/hdi-client.deb
+wget https://github.com/psf/azureARMFusion/blob/master/fusion-hdi-2.2.8-client-hdfs_2.6.6-SNAPSHOT-1548_all.deb\?raw\=true -O /tmp/hdi-client.deb
 
 dpkg -i /tmp/hdi-client.deb
 
